15842d0e976fb125d3d88bdb99504bef1f84dbf4
kdodeja62@gmail.com <kdodeja62@gmail.com>
2023-05-11 04:07:34 +0530

    Database Configuration for CURD

---
+++ springbootrestfuwebservices/src/main/java/com/springbootrestfuwebservices/SpringbootrestfuwebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.springbootrestfuwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootrestfuwebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootrestfuwebservicesApplication.class, args);
+	}
+
+}
---
+++ springbootrestfuwebservices/src/main/java/com/springbootrestfuwebservices/entity/User.java
@@ -0,0 +1,2 @@
+package com.springbootrestfuwebservices.entity;public class User {
+}
