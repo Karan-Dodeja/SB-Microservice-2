2dd65464e29e3941dbcb3f546db49e3128f6e63f
kdodeja62@gmail.com <kdodeja62@gmail.com>
2023-05-13 00:42:58 +0530

    Customize General Api Information

--- springbootrestfuwebservices/src/main/java/com/springbootrestfuwebservices/SpringbootrestfuwebservicesApplication.java
+++ springbootrestfuwebservices/src/main/java/com/springbootrestfuwebservices/SpringbootrestfuwebservicesApplication.java
@@ -1,11 +1,35 @@
 package com.springbootrestfuwebservices;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot Rest Api Documentation",
+				description = "Spring Boot Rest Api Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Karan Dodeja",
+						email = "kdodeja62@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot User Management Documentation"
+		)
+
+
+)
 public class SpringbootrestfuwebservicesApplication {
 
 	@Bean
