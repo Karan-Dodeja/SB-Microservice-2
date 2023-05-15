c8b608ee0649ba8ec7d583b66889867a69a8ce6c
kdodeja62@gmail.com <kdodeja62@gmail.com>
2023-05-16 02:09:04 +0530

    Service Registery For Multiple Instance

---
+++ serviceregistrey/src/main/java/com/serviceregistrey/ServiceregistreyApplication.java
@@ -0,0 +1,13 @@
+package com.serviceregistrey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceregistreyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceregistreyApplication.class, args);
+	}
+
+}
---
+++ serviceregistrey/src/test/java/com/serviceregistrey/ServiceregistreyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviceregistrey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceregistreyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
