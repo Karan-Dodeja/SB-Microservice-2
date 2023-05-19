8d4a8ec31de535310076a55fb5cf3b109e63a238
kdodeja62@gmail.com <kdodeja62@gmail.com>
2023-05-20 04:36:42 +0530

    Department Service General API Information

--- departmentservice/src/main/java/com/departmentservice/DepartmentserviceApplication.java
+++ departmentservice/src/main/java/com/departmentservice/DepartmentserviceApplication.java
@@ -1,11 +1,35 @@
 package com.departmentservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Department Service REST APIs",
+				description = "Department Service REST APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Karan",
+						email = "kdodeja62@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Department-Service Doc"
+		)
+)
 @SpringBootApplication
 public class DepartmentserviceApplication {
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DepartmentserviceApplication.class, args);
