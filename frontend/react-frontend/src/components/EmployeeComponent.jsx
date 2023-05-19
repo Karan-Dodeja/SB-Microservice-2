5431b00b61de0d2ec05cc4a63d8e6b05441ad5a2
kdodeja62@gmail.com <kdodeja62@gmail.com>
2023-05-19 22:32:21 +0530

    Show Details of Employee, Departement and Organization in Frontend

--- frontend/react-frontend/src/components/EmployeeComponent.jsx
+++ frontend/react-frontend/src/components/EmployeeComponent.jsx
@@ -28,7 +28,53 @@ class EmployeeComponent extends Component {
     render() {
         return (
             <div>
+                <div className="card column-md-6 offset-md-3">
+                    <h3 className='text-center card-header'> View Employee Details </h3>
+                    <div className='card-body'>
+                        <div className="row">
+                            <p><strong>Employee First Name: </strong>{this.state.employeeDto.firstName}</p>
+                        </div>
+                        <div className="row">
+                            <p><strong>Employee Last Name: </strong>{this.state.employeeDto.lastName}</p>
+                        </div>
+                        <div className="row">
+                            <p><strong>Employee Email: </strong>{this.state.employeeDto.email}</p>
+                        </div>
+                    </div>
+                </div>
 
+                <div className="card column-md-6 offset-md-3">
+                    <h3 className='text-center card-header'> View Department Details </h3>
+                    <div className='card-body'>
+                        <div className="row">
+                            <p><strong>Department Name: </strong>{this.state.departmentDto.departmentName}</p>
+                        </div>
+                        <div className="row">
+                            <p><strong>Department Description: </strong>{this.state.departmentDto.departmentDescription}</p>
+                        </div>
+                        <div className="row">
+                            <p><strong>Department Description: </strong>{this.state.departmentDto.departmentCode}</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="card column-md-6 offset-md-3">
+                    <h3 className='text-center card-header'> View Organization Details </h3>
+                    <div className='card-body'>
+                        <div className="row">
+                            <p><strong>Organization Name: </strong>{this.state.organization.organizationName}</p>
+                        </div>
+                        <div className="row">
+                            <p><strong>Organization Description: </strong>{this.state.organization.organizationDescription}</p>
+                        </div>
+                        <div className="row">
+                            <p><strong>Organization Code </strong>{this.state.organization.organizationCode}</p>
+                        </div>
+                        <div className="row">
+                            <p><strong>Organization Created At: </strong>{this.state.organization.createdDate}</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
