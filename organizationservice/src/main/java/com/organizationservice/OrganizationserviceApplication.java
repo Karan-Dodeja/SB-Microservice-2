901f3a345d8b177dcd4eb3c7fdc291f023f2ebe5
kdodeja62@gmail.com <kdodeja62@gmail.com>
2023-05-20 04:46:15 +0530

    General API Information

--- organizationservice/src/main/java/com/organizationservice/OrganizationserviceApplication.java
+++ organizationservice/src/main/java/com/organizationservice/OrganizationserviceApplication.java
@@ -1,9 +1,31 @@
 package com.organizationservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Employee Service REST APIs",
+				description = "Employee Service REST APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Karan",
+						email = "kdodeja62@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Employee-Service Doc"
+		)
+)
 public class OrganizationserviceApplication {
 
 	public static void main(String[] args) {
