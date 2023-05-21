362d4f5425da12ffb9c766c3f0e48dfeb3ed41a3
kdodeja62@gmail.com <kdodeja62@gmail.com>
2023-05-21 19:00:07 +0530

    Dockerfile

---
+++ docker/src/main/java/com/docker/controller/DockerController.java
@@ -0,0 +1,12 @@
+package com.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DockerController {
+    @GetMapping("/docker")
+    public String dockerDemo(){
+        return "Dockering Spring Boot Application";
+    }
+}
